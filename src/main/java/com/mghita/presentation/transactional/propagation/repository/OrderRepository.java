ae9d745a6df6d0aa10d503c83af7cd6b4dd1fa94
Madalin Marian Ghita <madalinmghita@gmail.com>
2021-08-26 20:02:50 +0300

    update on swagger

--- src/main/java/com/mghita/presentation/transactional/propagation/repository/OrderRepository.java
+++ src/main/java/com/mghita/presentation/transactional/propagation/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.mghita.presentation.transactional.propagation.repository;
 
 import com.mghita.presentation.transactional.propagation.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 }
